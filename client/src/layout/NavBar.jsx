e7c2901aed68a9e57d57246e3f34d51a8f926bfb
Maria Repina <MRepina@croc.ru>
2021-03-31 11:43:28 +0300

    Прикрутила лого

--- client/src/layout/NavBar.jsx
+++ client/src/layout/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Button, Menu, Container } from 'semantic-ui-react';
+import { Button, Menu, Container, Image } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import { history } from '../App';
 
-export default function NavBar() {
+export default function NavBar() {    
     return(
         <Menu fixed='top' inverted>
             <Container>
+                <Image src='../../../logo.png' size='small' style={{margin: '1em'}} centered />
                 <Menu.Item name='Authors' as={NavLink} exact to='/authors' />
                 <Menu.Item name='Books' as={NavLink} exact to='/books' />
                 <Menu.Item>
-                    <Button positive content='Logout' onClick={logout} ></Button>
+                    <Button positive content='Logout' onClick={logout}></Button>
                 </Menu.Item>
             </Container>
         </Menu>
