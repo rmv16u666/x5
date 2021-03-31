d7b10ea3d400c31c3970ced2b4b2132cb17e7a7a
Maria Repina <MRepina@croc.ru>
2021-03-31 11:12:26 +0300

    Update BookCard.jsx

--- client/src/features/BookCard.jsx
+++ client/src/features/BookCard.jsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from '@apollo/react-hooks';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card, Container, Header } from 'semantic-ui-react';
 import Comments from './Comments';
 
@@ -10,6 +11,7 @@ const BookCard = ({match: {params: {id}}}) => {
                 title
                 description
                 author {
+                    id
                     firstName
                     lastName
                 }
@@ -31,16 +33,20 @@ const BookCard = ({match: {params: {id}}}) => {
         return <Header as='h3'>Error while loading book card</Header>;
     }
 
-    const { title, description, author: { firstName, lastName } } = data?.getBook;
+    const { title, description, author: { id: authorId, firstName, lastName } } = data?.getBook;
 
     return(
         <Container>
             <Card style={{width: '100%'}}>
-                <Card.Content 
-                    header={title} 
-                    meta={`${firstName} ${lastName}`} 
-                    description={description} 
-                />
+                <Card.Content>
+                    <Card.Header>{title}</Card.Header>
+                    <Card.Meta>
+                        <Link to={`/authors/${authorId}`}>
+                            {`${firstName} ${lastName}`}
+                        </Link>
+                    </Card.Meta>
+                    <Card.Description>{description}</Card.Description>
+                </Card.Content>
             </Card>
             <Comments entityId={id} />
         </Container>
